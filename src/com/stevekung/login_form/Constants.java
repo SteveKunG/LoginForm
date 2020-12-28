09ed7ded3773cea71edb604caa3a74869fff69b5
Wasinthorn <mccommander_minecraft@hotmail.com>
2020-12-28 11:32:46 +0700

    Move to other repo

--- src/com/stevekung/login_form/Constants.java
+++ src/com/stevekung/login_form/Constants.java
@@ -6,7 +6,6 @@ public class Constants
 {
     public static final String API = "http://aritdoc.lpru.ac.th/api/api2/authentication"; // URL API ของ LPRU
     public static final String ALIVE = "http://aritdoc.lpru.ac.th/api/api2/alive"; // URL สำหรับเช็คว่า login อยู่หรือไม่
-    public static final String FILE_SERVICE = "http://aritdoc.lpru.ac.th/api/syn/new"; // URL สำหรับ upload file
 
     public static final Gson GSON = new Gson();
 }
